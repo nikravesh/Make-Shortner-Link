3bd9a517f16349ad5d629d2c1505ee1b4585f30d
Alireza Nikravesh <a.z.nikravesh@outlook.com>
2024-05-02 20:31:59 +0330

    Added badrequest test

--- ShortenerLink.Tests/UriShortenerControllerTests.cs
+++ ShortenerLink.Tests/UriShortenerControllerTests.cs
@@ -1,6 +1,14 @@
+using System.Web.Http.Results;
+
+using Microsoft.AspNetCore.Mvc;
+
 using Moq;
+
+using ShortenerLink.API.Core;
 using ShortenerLink.API.UriShortener;
+using ShortenerLink.BLL.UriShortener.Classes;
 using ShortenerLink.BLL.UriShortener.Interfaces;
+using ShortenerLink.Model.UriShortener.DTOs;
 
 namespace ShortenerLink.Tests;
 
@@ -10,12 +18,46 @@ public class UriShortenerControllerTests
     [InlineData("https://github.com/nikravesh/Make-Shortner-Link")]
     public async Task CreateShortenerUrlWhenCalled(string longUrl)
     {
-        Mock<IUriShortenerService> urlShortenerService = new();
-
-        var controller = new UriShortenerController(urlShortenerService.Object);
+        UriShortenerController controller = UriShortenerControllerTestsMotherObject.GetUriShortenerControllerObject;
 
         var response = await controller.CreateShortUrl(longUrl);
 
-        Assert.True(response!=null);
+        Assert.IsType<OkObjectResult>(response);
+        Assert.True(response != null);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    public async Task CreateShortenerUrlWithNullUrlWhenCalledReturnBadRequest(string url)
+    {
+        UriShortenerController controller = UriShortenerControllerTestsMotherObject.GetUriShortenerControllerObject;
+
+        var response = await controller.CreateShortUrl(url);
+
+        Assert.IsType<BadRequestObjectResult>(response);
+        Assert.Equal("url cannot be null!", APIErrorMessageConstants.BadRequestMessage);
+    }
+
+    [Fact]
+    public async Task CreateShortenerUrlWithEmptyUrlWhenCalledReturnBadRequest()
+    {
+        UriShortenerController controller = UriShortenerControllerTestsMotherObject.GetUriShortenerControllerObject;
+
+        var response = await controller.CreateShortUrl(string.Empty);
+
+        Assert.IsType<BadRequestObjectResult>(response);
+        Assert.Equal("url cannot be null!", APIErrorMessageConstants.BadRequestMessage);
+    }
+
+    [Theory]
+    [InlineData("https://github.com/nikravesh/Make Shortner Link")]
+    public async Task CreateShortenerUrlWithSpaceContainUrlWhenCalledReturnBadRequest(string url)
+    {
+        UriShortenerController controller = UriShortenerControllerTestsMotherObject.GetUriShortenerControllerObject;
+
+        var response = await controller.CreateShortUrl(string.Empty);
+
+        Assert.IsType<BadRequestObjectResult>(response);
+        Assert.Equal("url cannot be null!", APIErrorMessageConstants.BadRequestMessage);
     }
 }
---
+++ ShortenerLink.Tests/UriShortenerControllerTestsMotherObject.cs
@@ -0,0 +1,12 @@
+﻿using Moq;
+
+using ShortenerLink.API.UriShortener;
+using ShortenerLink.BLL.UriShortener.Interfaces;
+
+namespace ShortenerLink.Tests;
+
+public class UriShortenerControllerTestsMotherObject
+{
+    private static Mock<IUriShortenerService> UrlShortenerService => new();
+    public static UriShortenerController GetUriShortenerControllerObject => new(UrlShortenerService.Object);
+}
